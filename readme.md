69d6da0fc2c1be0acbbef439e7cac6b071d2d2a8
tashafmukhtar <tashafmukhtar@gmail.com>
2022-05-19 19:56:08 +0400

    final

---
+++ readme.md
@@ -0,0 +1,2 @@
+# Variables and Basic Types
+
---
+++ src/main/kotlin/DataTypes.kt
@@ -0,0 +1,25 @@
+fun main() {
+
+
+    val myFirstName = "Tashaf"
+    val myLastName = "Mukhtar"
+    var myJobTittle = "Kotlin Developer"
+    var mySalary  = 10000L
+    var  myAge = 30
+    var myFavouriteChar = 'S'
+
+    var favouriteNo = arrayOf(3,6,7,12)
+
+
+
+    println("My name is $myFirstName $myLastName my job is $myJobTittle and my salary $mySalary")
+    print("my age is $myAge my favourite char is $myFavouriteChar and my favouriteNo is ${favouriteNo[1]}")
+
+
+
+
+
+
+
+
+}
